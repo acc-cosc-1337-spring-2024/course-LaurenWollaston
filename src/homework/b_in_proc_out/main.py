54661b14e93badaa4e08d791264d1614227d7f6d
Lauren Wollaston <laurenwollaston@outlook.com>
2024-01-25 20:51:15 -0600

    homework 1

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,7 @@
 #
+import output
+# inpt1 = input("Enter value 1: ")
+# inpt2 = input('Enter Value 2: ') 
+# product = output.multiply_numbers(float(inpt1),float(inpt2))
+product = output.multiply_numbers(6,6)
+print(product)
