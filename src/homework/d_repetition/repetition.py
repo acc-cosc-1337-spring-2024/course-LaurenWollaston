4e5b8232139a3f1f3966866ab6e72d1dcedfd495
Lauren Wollaston <laurenwollaston@outlook.com>
2024-02-17 11:59:42 -0600

    homework 3

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,19 @@
-#
+def get_factorial(num):
+    factorial = 1
+    for num in range(1, num+1):
+        factorial *= num
+    # print ("FACTORIAL OF "+str(num)+" IS "+str(factorial))
+    return (factorial)
+
+def sum_odd_numbers(num):
+    i=0
+    sum=0
+    while (i<=num):
+        if (i%2 == 0):
+            i+=1
+        else:
+            sum +=num
+            i+=1
+    print (sum)
+    return sum
+sum_odd_numbers(7)
--- tests/homework/d_repetition/tests_repetition.py
+++ tests/homework/d_repetition/tests_repetition.py
@@ -1,1 +1,13 @@
-#
+import unittest 
+from src.homework.d_repetition.repetition import get_factorial
+
+class getFactorial(unittest.TestCase):
+
+    def test_getFactoral_1(self):
+        self.assertEqual(24, get_factorial(4))
+    
+    def test_getFactoral_2(self):
+        self.assertEqual(120, get_factorial(5))
+
+    def test_getFactoral_3(self):
+        self.assertEqual(720, get_factorial(6))
