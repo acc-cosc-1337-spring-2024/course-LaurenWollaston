a7b804469e0e7411a1e37f474f29d9acdd88ce77
Lauren Wollaston <laurenwollaston@outlook.com>
2024-02-17 12:05:40 -0600

    wrote tests for sumOddNumbers

--- tests/homework/d_repetition/tests_repetition.py
+++ tests/homework/d_repetition/tests_repetition.py
@@ -1,5 +1,5 @@
 import unittest 
-from src.homework.d_repetition.repetition import get_factorial
+from src.homework.d_repetition.repetition import get_factorial, sum_odd_numbers
 
 class getFactorial(unittest.TestCase):
 
@@ -11,3 +11,11 @@ class getFactorial(unittest.TestCase):
 
     def test_getFactoral_3(self):
         self.assertEqual(720, get_factorial(6))
+
+class sumOddNumbers(unittest.TestCase):
+    def test_sumOddNumbers_1(self):
+        self.assertEqual(16, sum_odd_numbers(7))
+    def test_sumOddNumbers_2(self):
+        self.assertEqual(25, sum_odd_numbers(9))
+    def test_sumOddNumbers_3(self):
+        self.assertEqual(25, sum_odd_numbers(10))
