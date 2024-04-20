aaa414ac75630a8ee8b1bc146906fd653cfb8c72
Lauren Wollaston <laurenwollaston@outlook.com>
2024-04-20 11:46:27 -0500

    Homework 9 - Classes

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,8 @@ has the test functions
 # from tests.homework.d_repetition import tests_repetition
 # from tests.homework.e_functions import tests_functions
 # from tests.homework.h_strings import tests_strings
-from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
+# from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
+from tests.homework.j_classes import tests_classes
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
+suite = unittest.TestLoader().loadTestsFromModule(tests_classes)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,60 @@
-#
+from class_a import die
+power = 1
+dice = die()
+
+
+def selectMenu(select):
+    try:
+        (int(select)==1 or int(select)==2)
+        if int(select)==1:
+            return(2)
+        elif int(select)==2:
+            return(3)
+        else:
+            print('Error: please enter one of the listed options')
+            return(1)
+    except:
+        print('Error - Please enter one of the listed options.')
+        return(1)
+
+def quitMenu(select):
+    try:
+        if select.upper()=='Y' or select.upper()=='N' or select=='1' or select=='2':
+            if(select=='1' or select.upper()=='Y'):
+                return(0)
+            elif(select=='2' or str(select).upper()=='N'):
+                return(1)
+            else:
+                print('Error: an unexpected error occurred.')
+                return(1)
+        else:
+            print('Error: please enter one of the listed options')
+            return(1)
+    except:
+        print('Error: please enter one of the listed options')
+        return(1)
+
+while power == 1:
+    print('')
+    print('1 - Roll Dice')
+    print('2 - Quit')
+    print('________________')
+    print('')
+    select = input('')
+    power = selectMenu(select)
+    while power ==2:
+        print('')
+        dice.roll()
+        print(dice)
+        print('')
+        print('Enter 1 to roll again, or press 2 to quit.')
+        select = input('')
+        power = selectMenu(select)
+    
+    while power == 3:
+        print('')
+        print('Would you really like to quit?')
+        print('To confirm enter y, or 1. To return to the program, enter n or 2')
+        print('')
+        select = input('')
+        power = quitMenu(select)
